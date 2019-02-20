78f06dc2ad999dc9631a60327a9e93ea24b81bf3
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2019-02-20 14:59:17 +0100

    refactoring to a recursive method

--- FizzBuzz_CSharp/Program.cs
+++ FizzBuzz_CSharp/Program.cs
@@ -10,23 +10,50 @@ namespace FizzBuzz_CSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Foor loop solution");
-            string text;
-            for (int i = 1; i <= 100; i++)
-            {
-                if (i % 3 == 0 && i % 5 == 0)
-                    text = "FizzBuzz";
-                else if (i % 3 == 0)
-                    text = "Fizz";
-                else if (i % 5 == 0)
-                    text = "Buzz";
-                else
-                    text = i.ToString();
+            //Console.WriteLine("Loop solution");
 
-                Console.WriteLine(text);
-            }
+            //string text;
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 3 == 0 && i % 5 == 0)
+            //        text = "FizzBuzz";
+            //    else if (i % 3 == 0)
+            //        text = "Fizz";
+            //    else if (i % 5 == 0)
+            //        text = "Buzz";
+            //    else
+            //        text = i.ToString();
+
+            //    Console.WriteLine(text);
+            //}
+
+
+            Console.WriteLine("Recursive Solution");
+            RecursiveSolution(1);
             Console.ReadKey();
 
         }
+
+        private static void RecursiveSolution(int n)
+        {
+            string text;
+            if (n > 100)
+                return;
+
+            if (n % 3 == 0 && n % 5 == 0)
+                text = "FizzBuzz";
+            else if (n % 3 == 0)
+                text = "Fizz";
+            else if (n % 5 == 0)
+                text = "Buzz";
+            else
+                text = n.ToString();
+
+            Console.WriteLine(text);
+
+            RecursiveSolution(n + 1);
+
+        }
+
     }
 }
