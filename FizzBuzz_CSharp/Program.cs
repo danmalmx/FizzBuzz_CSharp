38d699e1413abe22b69bc84b203d2bc10f6da010
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2019-02-20 14:45:48 +0100

    adding for loop solution

--- FizzBuzz_CSharp/Program.cs
+++ FizzBuzz_CSharp/Program.cs
@@ -10,7 +10,23 @@ namespace FizzBuzz_CSharp
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Foor loop solution");
+            string text;
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                    text = "FizzBuzz";
+                else if (i % 3 == 0)
+                    text = "Fizz";
+                else if (i % 5 == 0)
+                    text = "Buzz";
+                else
+                    text = i.ToString();
+
+                Console.WriteLine(text);
+            }
+            Console.ReadKey();
+
         }
     }
 }
